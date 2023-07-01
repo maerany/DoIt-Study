87d2da49743a075c0f50ce1cb1531e93c7ccc250
maerany <88430300+maerany@users.noreply.github.com>
2023-07-01 23:59:30 +0900

    문제016 - 버블소트 프로그램 (bubble sort)

---
+++ doitStudy/sortingAlgorithm/bubbleSort/P1377_버블소트1.java
@@ -0,0 +1,47 @@
+package doitStudy.sortingAlgorithm.bubbleSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P1377_버블소트1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 배열의 크기
+        int N = Integer.parseInt(br.readLine());
+        mData[] A = new mData[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = new mData(Integer.parseInt(br.readLine()), i);
+        }
+
+        Arrays.sort(A);
+        int Max = 0;
+        for (int i = 0; i < N; i++) {
+            if (Max < A[i].index - i) {
+                Max = A[i].index - i;
+            }
+        }
+
+        System.out.println(Max + 1);
+    }
+}
+
+class mData implements Comparable<mData>{
+    int value;
+    int index;
+
+    public mData(int value, int index){
+        super();
+        this.value = value;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(mData o){
+        return this.value - o.value;
+    }
+}
